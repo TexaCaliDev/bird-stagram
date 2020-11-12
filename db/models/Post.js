5c837b8eba67ccc0c15124ecbecd9ed99e7bf722
benjmain roth <benjaminsavanne@gmail.com>
2020-11-11 18:29:50 -0600

    made pictures required on picture in Post.js

--- db/models/Post.js
+++ db/models/Post.js
@@ -1,21 +1,22 @@
-const {Schema} = require('mongoose')
+const {
+    Schema
+} = require('mongoose')
 
-module.exports = new Schema(
-    {
-        picture: {
-            type: String
-        },
-        birdName: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String,
-            required: true
-        },
-        Region: {
-            type: String,
-            required: true
-        }
+module.exports = new Schema({
+    picture: {
+        type: String,
+        required: true
+    },
+    birdName: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    Region: {
+        type: String,
+        required: true
     }
-)
+})
