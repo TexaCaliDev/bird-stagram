7d8148bf1f876c26e94141a514bb651b22da1d51
benjmain roth <benjaminsavanne@gmail.com>
2020-11-12 14:23:27 -0600

    adjusted usercontroller to be functional

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,5 @@
-const { request } = require('express')
-const { User,Post,FavBird } = require('../db/schema')
+
+const { User} = require('../db/schema')
 const jwt = require('jsonwebtoken')
 const {
   checkPassword,
@@ -8,20 +8,16 @@ const {
 
 const GetProfile = async (req, res) => {
   try {
-    const user = await User.findById(req.params.user_id).select('_id name')
-    const posts = await Post.find({
-      user_id: req.params.user_id
-    })
-    res.send({
-      user,
-      posts
-    })
+    const user = await User.findById(req.params.user_id)
+   
+    res.send({ user})
   } catch (error) {
     throw error
   }
 }
 
 const CreateUser = async (req, res) => {
+  console.log(req.body)
   try {
     const body = req.body
     const password_digest = await generatePassword(body.password)
@@ -62,9 +58,20 @@ const SignInUser = async (req, res, next) => {
 }
 
 
+const RefreshSession = (req, res) => {
+  try {
+    const token = res.locals.token
+    res.send({ user: jwt.decode(token), token: res.locals.token })
+  } catch (error) {
+    throw error
+  }
+}
+
+
 module.exports = {
   GetProfile,
   CreateUser,
   SignInUser,
+  RefreshSession
 
 }
