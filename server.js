40eba3eb75e40d214b20b3ed871c31b8f515b1b5
benjmain roth <benjaminsavanne@gmail.com>
2020-11-12 13:41:02 -0600

    server is listening on port

--- server.js
+++ server.js
@@ -1,35 +1,33 @@
-const AppRouter = require('./routes/AppRouter')
-const express = require('express')
-const logger = require('morgan')
+const app = require('express')()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const helmet = require('helmet')
+const morgan = require('morgan')
 const connection = require('./db/connection')
-
-const PORT = process.env.PORT || 3001
-const app = express()
-
+const AppRouter = require('./routes/AppRouter')
+const PORT = process.env.PORT || 3004
+const helmet = require('helmet')
 // Initialize Middleware
-app.use(logger('dev'))
+app.use(morgan('dev'))
 app.use(helmet())
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  extended: true
-}))
-// Initialize Middleware
-app.disable('X-Powered-By')
+    extended: true
+  }))
+  // Initialize Middleware
+  
 app.get('/', (req, res) => res.send({
-  msg: 'Server Working'
+  msg: 'oh yea'
 }))
 app.use('/api', AppRouter)
 
 app.listen(PORT, async () => {
   try {
     await connection
-    console.log('Database Connected')
+    console.log('Database connected')
     console.log(`App listening on port: ${PORT}`)
-  } catch (error) {
-    throw new error('Connection Error')
+  } catch(error) {
+    throw new Error(error)
   }
 })
+
